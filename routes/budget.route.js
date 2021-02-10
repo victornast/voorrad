74d4d46bd748c2cdbeea57ff5e9ed4e610a0e1b8
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2021-02-10 11:43:00 +0100

    POST router for add new category form

--- routes/budget.route.js
+++ routes/budget.route.js
@@ -45,6 +45,20 @@ router.get('/categories', routeGuard, (req, res, next) => {
 router.get('/add-category', routeGuard, (req, res, next) => {
   res.render('transactions/add-category');
 });
+// THIS DOES NOT WORK AS EXPECTED - DOES NOT STORE NEW CATEGORY IN DB
+router.post('/add-category', routeGuard, (req, res, next) => {
+  const data = req.body;
+  Category.create({
+    label: data.label,
+    name: data.name
+  })
+    .then(() => {
+      res.redirect('/budget/categories');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 router.post('/categories', routeGuard, (req, res, next) => {
   res.redirect('/budget/categories');
