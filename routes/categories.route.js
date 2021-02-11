014a263436079561c9332745fe514412f143e514
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2021-02-11 20:31:50 +0100

    enabling editing of categories

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -6,6 +6,7 @@ const routeGuard = require('../middleware/route-guard');
 // const Transaction = require('./../models/transactions.model');
 const Category = require('./../models/categories.model');
 // const Budget = require('./../models/budget.model');
+const mongoose = require('mongoose');
 
 router.get('/categories', routeGuard, (req, res, next) => {
   Category.find({ budgetId: req.user.budgetId })
@@ -30,6 +31,32 @@ router.post('/add-category', routeGuard, (req, res, next) => {
     label: data.label,
     name: data.name,
     budgetId: req.user.budgetId,
+    plannedAmount: data.plannedAmount
+  })
+    .then(() => {
+      res.redirect('/budget/categories');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+router.get('/edit-category/:id', routeGuard, (req, res, next) => {
+  const id = req.params.id;
+  Category.findById(id)
+    .then((category) => {
+      res.render('transactions/edit', { category });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post('/edit-category/:id', routeGuard, (req, res, next) => {
+  const data = req.body;
+  const id = req.params.id;
+  console.log(data, id);
+  Category.findByIdAndUpdate(id, {
+    name: data.name,
     plannedAmount: data.plannedAmount
   })
     .then(() => {
