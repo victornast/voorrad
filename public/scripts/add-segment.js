78d780822a81ccb98562025a523ea8b1b9c1f1c6
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2021-02-18 21:23:03 +0100

    enabling the user to split transaction

--- public/scripts/add-segment.js
+++ public/scripts/add-segment.js
@@ -1,1 +1,9 @@
+const dataSegment = document.querySelector('[data-segment]');
 const segmentGroup = document.getElementById('segment-group');
+const splitTransaction = document.getElementById('split-button');
+
+splitTransaction.addEventListener('click', () => {
+  let dynamicDiv = document.createElement('div');
+  dynamicDiv.innerHTML = dataSegment.innerHTML;
+  segmentGroup.append(dynamicDiv);
+});
